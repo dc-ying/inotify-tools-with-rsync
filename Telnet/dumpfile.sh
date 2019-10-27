0a52f086b2e60f35e614221ff71b3f97132b50c1
Zhengtya <yangzhengtang@sjtu.edu.cn>
2019-10-27 09:43:35 +0800

    Add telnet_server
      Changes to be committed:
    	modified:   Dockerfile
    	new file:   Telnet/Dockerfile
    	new file:   Telnet/dumpfile.sh
    	new file:   Telnet/telnet

---
+++ Telnet/dumpfile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+xinetd -dontfork -stayalive &
+
+eth=$1
+listen_port=$2
+work_dir=$3
+
+echo "catching $eth:$listen_port"
+mkdir $work_dir/dumps
+
+times=0
+while :
+do
+	#echo "Now $times"
+	timestamp=`date "+%Y-%m-%d_%H:%M:%S"`
+	let times+=1
+	tcpdump tcp -i $eth -c 100 -w $work_dir/dumps/log_$timestamp_$times.pcap port $listen_port > /dev/null  2>&1
+done
